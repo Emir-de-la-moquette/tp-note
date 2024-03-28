2383e5eb7a6b75f4e390398f100e329e2185ab97
Joaquin "Joe" Lapin <Jo.Lapin@univ-hochmoehrefelder.fr>
2024-03-28 10:58:18 +0100

    ajout des tests de prix de revient

--- src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
+++ src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
@@ -3,9 +3,11 @@ package fr.univ_orleans.iut45.menus;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
+
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class AppTest
@@ -58,6 +60,72 @@ public class AppTest
         les_plats_principaux.add(tortilla);
         Menu ce_soir = new Menu(les_entrees, les_plats_principaux, les_desserts);
         assertTrue(ce_soir.estComplet());
+
     }
 
+
+// TEST PRIX DE REVIENT QUESTION 8
+@Test
+    public void testPrixMinMaxMenu()
+    {
+        Plat carottes_rapees = new Plat("Carottes Rapées", false, new ArrayList<String>(), 0.5);
+        Plat acier = new Plat("acier", false, new ArrayList<String>(), 69.5);
+        Plat choux = new Plat("choux", false, new ArrayList<String>(), 1.5);
+        Plat charcuterie = new Plat("charcuterie", true, new ArrayList<String>(), 7.5);
+
+        Plat pouletCurry = new Plat("Poulet au Curry", true, new ArrayList<String>(), 12.00);
+        Plat saladeNicoise = new Plat("Salade Niçoise", false, new ArrayList<String>(), 10.5);
+        Plat risottoFruitsMer = new Plat("Risotto aux Fruits de Mer", false, new ArrayList<String>(), 15.50);
+        Plat lasagnesVegetariennes = new Plat("Lasagnes Végétariennes", false, new ArrayList<String>(), 11.25);
+        Plat tajineAgneauPruneaux = new Plat("Tajine d'Agneau aux Pruneaux", false, new ArrayList<String>(), 14.00);
+        Plat pizzaQuatreFromages = new Plat("Pizza Quatre Fromages", true, new ArrayList<String>(), 9.99);
+        Plat sushiAssortis = new Plat("Sushi Assortis", true, new ArrayList<String>(), 18.50);
+        Plat soupePoisson = new Plat("Soupe de Poisson", false, new ArrayList<String>(), 8.25);
+        Plat gratinPommesTerre = new Plat("Gratin de Pommes de Terre", false, new ArrayList<String>(), 9.5);
+        Plat penneArrabiata = new Plat("Penne Arrabiata", false, new ArrayList<String>(), 10.20);
+
+        Plat fondantChocolat = new Plat("Fondant au Chocolat", false, new ArrayList<String>(), 8.00);
+        Plat crèmeBrûlée = new Plat("Crème Brûlée", false, new ArrayList<String>(), 7.50);
+        Plat tarteFruitsRouges = new Plat("Tarte aux Fruits Rouges", false, new ArrayList<String>(), 10.5);
+        Plat mousseMangue = new Plat("Mousse à la Mangue", false, new ArrayList<String>(), 6.5);
+        Plat crumblePommes = new Plat("Crumble aux Pommes", false, new ArrayList<String>(), 9.5);
+
+
+
+        ArrayList<Plat> les_entrees_midi = new ArrayList<Plat>();
+        ArrayList<Plat> les_plats_midi = new ArrayList<Plat>();
+        ArrayList<Plat> les_desserts_midi = new ArrayList<Plat>();
+
+        les_entrees_midi.add(carottes_rapees); 
+        les_entrees_midi.add(choux); 
+        les_plats_midi.add(risottoFruitsMer);
+        les_plats_midi.add(tajineAgneauPruneaux);
+        les_desserts_midi.add(crumblePommes);
+        les_desserts_midi.add(tarteFruitsRouges);
+        les_desserts_midi.add(mousseMangue);
+
+        Menu ce_midi = new Menu(les_entrees_midi,  les_plats_midi, les_desserts_midi);
+
+        ArrayList<Plat> les_entrees_soir = new ArrayList<Plat>();
+        ArrayList<Plat> les_plats_soir = new ArrayList<Plat>();
+        ArrayList<Plat> les_desserts_soir = new ArrayList<Plat>();
+
+        les_entrees_soir.add(acier);
+        les_entrees_soir.add(charcuterie);
+        les_plats_soir.add(pouletCurry);
+        les_plats_soir.add(tajineAgneauPruneaux);
+        les_plats_soir.add(gratinPommesTerre);
+        les_plats_soir.add(lasagnesVegetariennes);
+        les_desserts_soir.add(crumblePommes);
+        les_desserts_soir.add(crèmeBrûlée);
+        les_desserts_soir.add(fondantChocolat);
+
+        Menu ce_soir = new Menu(les_entrees_soir,  les_plats_soir, les_desserts_soir);
+
+
+        assertEquals(17.5, ce_midi.prixRevientMin());
+        assertEquals(26.0, ce_midi.prixRevientMax());
+        assertEquals(24.5, ce_soir.prixRevientMin());
+        assertEquals(93.0, ce_midi.prixRevientMax());
+    }
 }
